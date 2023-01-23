da85548c8f0142fa724c84cb523ec1d76c751993
Manuela Sartor <manuelasartor2015@gmail.com>
2023-01-23 14:04:17 -0300

    La geolocalización funciona pero las coordenadas provenientes de la API no coinciden con el país especificado.

--- script_CV.js
+++ script_CV.js
@@ -4,22 +4,97 @@ function init () {
     const user_url = 'https://randomuser.me/api/?exc=login,registered,id';
     fetchData (user_url);
     crearTitulo ();
+    document.getElementById('nombre').addEventListener('focusout', localStorage_nombre);
+    document.getElementById('apellido').addEventListener('focusout', localStorage_apellido);
+    document.getElementById('email').addEventListener('focusout', localStorage_email);
+    document.getElementById('url').addEventListener('focusout', localStorage_url);
+    document.getElementById('ciudad').addEventListener('focusout', localStorage_ciudad);
+    document.getElementById('provincia').addEventListener('focusout', localStorage_provincia);
+    document.getElementById('codigoPostal').addEventListener('focusout', localStorage_codigoPostal);
+    document.getElementById('motivo').addEventListener('focusout', localStorage_motivoConsulta);
+ }
 
 
 
-//  document.getElementById('enviar').addEventListener('click', mostrar_localStorage);
-document.getElementById('nombre').addEventListener('focusout', localStorage_nombre);
-document.getElementById('apellido').addEventListener('focusout', localStorage_apellido);
-document.getElementById('email').addEventListener('focusout', localStorage_email);
-document.getElementById('url').addEventListener('focusout', localStorage_url);
-document.getElementById('ciudad').addEventListener('focusout', localStorage_ciudad);
-document.getElementById('provincia').addEventListener('focusout', localStorage_provincia);
-document.getElementById('codigoPostal').addEventListener('focusout', localStorage_codigoPostal);
-document.getElementById('motivo').addEventListener('focusout', localStorage_motivoConsulta);
-
-}
 
 
+ 
+function fetchData (url) { //traer los datos del CV de la persona de la API (de la url definida como constante en la función init) y mostrarlos en el HTML.
+  console.log('start fetch');
+  fetch(url).then(response => {
+    console.log('response: ');
+    console.log(response);
+    if (!response.ok) //si el estatus del pedido no es "ok: true", entonces dar un mensaje de error.
+      throw Error('ERROR')
+    return data = response.json();
+  }).then(data => {
+      console.log('data: ');
+      console.log(data);
+      const nombre = data.results.map(user => {
+        return data.results[0].name.first + " " + data.results[0].name.last;
+      })
+      document.querySelector('#obj-name').innerHTML = nombre;
+
+      const foto = data.results.map(user => {
+        return data.results[0].picture.large;
+      })
+      document.querySelector('#divFoto').innerHTML = '<img alt="foto de perfil" id="p-img" src="' + foto  + '" >'
+
+      const email = data.results.map(user => {
+        return data.results[0].email;
+      })
+      document.querySelector('#p-email').innerHTML = '<p> <span> e-mail: </span> <br>' + email + '</p>';
+
+      const telefono = data.results.map(user => {
+        return data.results[0].phone;
+      })
+      document.querySelector('#p-phone').innerHTML = '<p> <span> Telefono: </span> <br>' + telefono + '</p>';
+
+      const calle_nro = data.results.map(user => {
+        return data.results[0].location.street.number
+      })
+
+      const calle_nombre =  data.results.map(user => {
+        return data.results[0].location.street.name
+      })
+      const ciudad =  data.results.map(user => {
+        return data.results[0].location.city
+      })
+      const pais =  data.results.map(user => {
+        return data.results[0].location.country
+      })
+      const codigo_postal =  data.results.map(user => {
+        return data.results[0].location.postcode
+      })
+      document.querySelector('#p-adress').innerHTML = '<p> <span> Dirección: </span> <br>' + calle_nombre + " " + calle_nro + ", " + ciudad + ", " + pais + ". CP:" + codigo_postal + '</p>';
+      let latitudCV = data.results.map(user => { //obtener las coordenadas para la latitud de la persona del CV.
+        return data.results[0].location.coordinates.latitude;
+      }) 
+      let longitudCV = data.results.map(user => { //obtener las coordenadas para la longitud de la persona del CV.
+        return data.results[0].location.coordinates.longitude;
+      })
+      geolocalizacionCV (latitudCV, longitudCV);
+    }).catch(error => {
+    console.log(error);
+  })
+  } 
+
+
+  function crearTitulo () {
+    // arrays para randomizar el título universitario del CV de la persona:
+    const preTitulo = ["Ing.", "Lic.", "Técnico"]
+    const titulo = ["en Informática", "Agronomo", "en Sistemas", "Industrial", "en Desarrollo", "en Biotecnología", "Electrónico"]
+    // mostrar el título en el CV:
+    var preIndex = Math.round(Math.random()*(preTitulo.length -1));
+    var pre = preTitulo[preIndex];
+    console.log('Pre titulo: ' + pre);
+    var tituloIndex = Math.round(Math.random()*(titulo.length -1));
+    var Titulo = titulo[tituloIndex];
+    console.log('Titulo: ' + Titulo);
+    document.querySelector('#titulo').innerHTML = pre + " " + Titulo;
+  }
+
+// funciones para guardar los datos del formulario en localStorage:
 function localStorage_nombre() {
   if (localStorage.nombre) {
     console.log('Nombre guardado: ' + localStorage.nombre);
@@ -93,97 +168,29 @@ function localStorage_motivoConsulta () {
     console.log('motivo consulta: ' + localStorage.motivoConsulta);
   }
 }
-
  
-function fetchData (url) { //traer los datos del CV de la persona de la API (de la url definida como constante en la función init) y mostrarlos en el HTML.
-  console.log('start fetch');
-  fetch(url).then(response => {
-    console.log('response: ');
-    console.log(response);
-    if (!response.ok) //si el estatus del pedido no es "ok: true", entonces dar un mensaje de error.
-      throw Error('ERROR')
-    return data = response.json();
-  }).then(data => {
-      console.log('data: ');
-      console.log(data);
-      const nombre = data.results.map(user => {
-        return data.results[0].name.first + " " + data.results[0].name.last;
-      })
-      document.querySelector('#obj-name').innerHTML = nombre;
-
-      const foto = data.results.map(user => {
-        return data.results[0].picture.large;
-      })
-      document.querySelector('#divFoto').innerHTML = '<img alt="foto de perfil" id="p-img" src="' + foto  + '" >'
-
-      const email = data.results.map(user => {
-        return data.results[0].email;
-      })
-      document.querySelector('#p-email').innerHTML = '<p> <span> e-mail: </span> <br>' + email + '</p>';
-
-      const telefono = data.results.map(user => {
-        return data.results[0].phone;
-      })
-      document.querySelector('#p-phone').innerHTML = '<p> <span> Telefono: </span> <br>' + telefono + '</p>';
-
-      const calle_nro = data.results.map(user => {
-        return data.results[0].location.street.number
-      })
-
-      const calle_nombre =  data.results.map(user => {
-        return data.results[0].location.street.name
-      })
-      const ciudad =  data.results.map(user => {
-        return data.results[0].location.city
-      })
-      const pais =  data.results.map(user => {
-        return data.results[0].location.country
-      })
-      const codigo_postal =  data.results.map(user => {
-        return data.results[0].location.postcode
-      })
-
-      document.querySelector('#p-adress').innerHTML = '<p> <span> Dirección: </span> <br>' + calle_nombre + " " + calle_nro + ", " + ciudad + ", " + pais + ". CP:" + codigo_postal + '</p>';
-  }).catch(error => {
-    console.log(error);
-  })
-  } 
-
-
-  function crearTitulo () {
-    // arrays para randomizar el título universitario del CV de la persona:
-    const preTitulo = ["Ing.", "Lic.", "Técnico"]
-    const titulo = ["en Informática", "Agronomo", "en Sistemas", "Industrial", "en Desarrollo", "en Biotecnología", "Electrónico"]
-    // mostrar el título en el CV:
-    var preIndex = Math.round(Math.random()*(preTitulo.length -1));
-    var pre = preTitulo[preIndex];
-    console.log('Pre titulo: ' + pre);
-    var tituloIndex = Math.round(Math.random()*(titulo.length -1));
-    var Titulo = titulo[tituloIndex];
-    console.log('Titulo: ' + Titulo);
-    document.querySelector('#titulo').innerHTML = pre + " " + Titulo;
-  }
-
-
- 
-
+// ----
   
 
-  // -------
 
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
+// Get current position successfully
+function geolocalizacionCV (latitudCV, longitudCV) {
+  console.log('latitudCV: ' + latitudCV + ", longitudCV: " + longitudCV);
+  console.log('typeoff latitudCV = ' + typeof(latitudCV) );
+  console.log(latitudCV);
+  const lat = JSON.parse(latitudCV);
+  console.log('typeoff lat = ' + typeof(lat) );
+  console.log(lat);
+  const long = JSON.parse(longitudCV);
+  console.log('typeoff long = ' + typeof(long) );
+  console.log(long);
+
+	const map = L.map('map').setView([lat, long], 13); // las coordenadas provistas por la API no concuerdan con el país.
+
+  var marker = L.marker([lat, long]).addTo(map);
+
+  const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	 	maxZoom: 19,
+	 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	 }).addTo(map);
 }
-
-function showPosition(position) {
-    console.log("Latitud: " + position.coords.latitude)
-    console.log("Longitud: " + position.coords.longitude);
-
-  x.innerHTML = "Latitud: " + position.coords.latitude + 
-  "<br>Longitud: " + position.coords.longitude;
-}
-
